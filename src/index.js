7ab8c536bd1d5362b9e9ff7f1cc1f9aa53a5a6c7
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-09-30 13:13:25 +0100

    Fixed changes reviewer requested

--- src/index.js
+++ src/index.js
@@ -1,75 +1,20 @@
-/* eslint-disable no-unused-vars */
 import './style.css';
-import {
-  taskObjects,
-  clearCompleted,
-  highlightTask,
-  editTask,
-  removeTask,
-  taskDisplay,
-  addToLocaleStorage,
-  getTask,
-} from '../modules/functions.js';
+import Task from '../modules/task.js';
 
-window.onload = getTask();
+const task = new Task();
+window.addEventListener('DomContentLoaded', task.showAllTasks());
 
-const addTask = document.querySelector('#add');
-addTask.addEventListener('keypress', (e) => {
+const clearBtn = document.querySelector('#btn');
+const addTaskInput = document.getElementById('task-input');
+const addBtn = document.getElementById('add-btn');
+
+addBtn.addEventListener('click', () => task.addTasks());
+
+addTaskInput.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     e.preventDefault();
-    if (addTask.value !== '') {
-      // eslint-disable-next-line no-undef
-      e = taskObjects(addTask.value);
-      addTask.value = '';
-    } else {
-      e.preventDefault();
-    }
+    task.addTasks();
   }
 });
 
-// Event Handlers for the remove and highlight functions
-document.addEventListener('click', (e) => {
-  if (!(e.target.matches('.checkTask') || e.target.matches('.fa-trash'))) {
-    return;
-  }
-  if (e.target.matches('.checkTask')) {
-    const tasks = document.querySelectorAll('.checkTask');
-    tasks.forEach((task, index) => {
-      if (e.target === task) {
-        highlightTask(index);
-      }
-    });
-  } else {
-    const deleteBtn = document.querySelectorAll('.fa-trash');
-    deleteBtn.forEach((btn, index) => {
-      if (e.target === btn) {
-        removeTask(index);
-      }
-    });
-  }
-});
-
-// Event handler for the edit task function
-document.addEventListener('change', (e) => {
-  if (!e.target.matches('.task-value')) {
-    return;
-  }
-  const allTasks = document.querySelectorAll('.task-value');
-  allTasks.forEach((task, index) => {
-    if (e.target === task) {
-      editTask(task.value, index);
-    }
-  });
-});
-
-// Event Handler for the clearCompleted function
-const clearAll = document.querySelector('.clear-btn');
-clearAll.addEventListener('click', () => {
-  // const taskStatus = document.querySelector('.checkTask');
-  // const taskStatusArray = Array.from(taskStatus);
-  // taskStatusArray.forEach((task, index) => {
-  //   if (taskStatus.checked === true) {
-  clearCompleted();
-  //   }
-  // });
-});
+clearBtn.addEventListener('click', () => task.clearComplete());
