14c04a3d0e7eeb9e380f1c40b07b452a98fa3489
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-09-30 13:12:26 +0100

    Fixed changes reviewer asked

---
+++ modules/eventlisteners.js
@@ -0,0 +1,24 @@
+export default function addEventListenersToTheElements(task) {
+  const listWrapper = document.querySelectorAll('.check');
+  const completed = document.querySelectorAll('.completed');
+  const btn = document.querySelectorAll('.del-btn');
+
+  completed.forEach((item) => {
+    item.addEventListener('click', () => {
+      task.completed(item);
+    });
+  });
+
+  listWrapper.forEach((item) => {
+    item.addEventListener('click', () => {
+      task.editTask(item.id, item.innerText);
+    });
+  });
+
+  btn.forEach(
+    (item) => item.addEventListener(
+      'click',
+      () => task.deleteTask(item.classList[item.classList.length - 1]),
+    ),
+  );
+}
