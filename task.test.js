f0079353ed6d8082240e60a55bdc53b26f2ee39a
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-10-06 11:34:56 +0100

    Modify the test file

--- task.test.js
+++ task.test.js
@@ -2,42 +2,18 @@
  * @jest-environment jsdom
  */
 
-// test('Testing', () => {
-//   document.body.innerHTML = `
-//   <input id='task-input'>
-//   <button id='add-btn'>Add task</button>
-//   <ul id='list-ul'></ul>
-//   `
-
-//   const Task = require('./modules/task');
-//   const task = new Task();
-
-//   const input = document.getElementById('task-input');
-//   const btn = document.getElementById('add-btn');
-//   const ul = document.getElementById('list-ul');
-
-//   input.value = 'Task 1';
-//   task.addTasks();
-//   input.value = 'Task 2';
-//   task.addTasks();
-//   input.value = 'Task 3';
-//   task.addTasks();
-//   const children = ul.querySelectorAll('li');
-//   expect(children).toHaveLength(3);
-// })
-
 describe('Add and Remove Testing', () => {
   document.body.innerHTML = `
   <input id='task-input'>
   <button id='add-btn'>Add task</button>
   <ul id='list-ul'></ul>
-  `
+  `;
 
+  // eslint-disable-next-line global-require
   const Task = require('./modules/task');
   const task = new Task();
 
   const input = document.getElementById('task-input');
-  const btn = document.getElementById('add-btn');
   const ul = document.getElementById('list-ul');
 
   test('Add', () => {
@@ -48,9 +24,8 @@ describe('Add and Remove Testing', () => {
     input.value = 'Task 3';
     task.addTasks();
     const children = ul.querySelectorAll('li');
-    console.log(ul.innerHTML);
     expect(children).toHaveLength(3);
-  })
+  });
 
   test('Remove', () => {
     const btn = document.querySelectorAll('.del-btn');
@@ -58,7 +33,6 @@ describe('Add and Remove Testing', () => {
     btn[1].click();
     btn[2].click();
     const children = ul.querySelectorAll('li');
-    console.log(ul.innerHTML);
     expect(children).toHaveLength(0);
-  })
-})
+  });
+});
